25a8901c03fe88323518bbf4921ac2d9d6fa08fe
jobird <jonathanchanwj@gmail.com>
2023-05-08 22:13:28 +0900

    initial structure commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lifebit-tech-challenge
 lifebit-tech-challenge
+
+![architecture](architecture.drawio)
---
+++ alb.tf
@@ -0,0 +1,42 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = "vpc-abcde012"
+  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+  security_groups    = ["sg-edcd9784", "sg-edcd9785"]
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = "i-0123456789abcdefg"
+          port = 80
+        }
+        my_other_target = {
+          target_id = "i-a1b2c3d4e5f6g7h8i"
+          port = 8080
+        }
+      }
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = locals.tags
+}
